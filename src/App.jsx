7cab182550966da4b00efb0d70a806a195b5308f
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-07-11 23:33:40 -0400

    change color

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { TodoProvider } from "./context";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
